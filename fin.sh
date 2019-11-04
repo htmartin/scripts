cf086c8d6ae1c11f612e0a5ed9b1c5df0878c1e2
htmartin <helen.taylor.martin@gmail.com>
2019-11-04 15:08:14 -0700

    opwn

---
+++ fin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+open https://mint.com/
+
+open https://docs.google.com/spreadsheets/d/1KfmbPV7ZWOkKK1IYnfGv-r0UBd7KmUjUYKqaJiqvp20/edit#gid=555422107
+
+open https://lastpass.com/
+
+open https://www.usaa.com/inet/ent_logon/Logon?logoffjump=true
+
+open https://www.amazon.com/
