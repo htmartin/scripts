819400aada7b0609cfc216dfe402ff46c4ce11ff
htmartin <helen.taylor.martin@gmail.com>
2019-11-04 15:19:31 -0700

    open

---
+++ class.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+open https://docs.google.com/spreadsheets/d/1KfmbPV7ZWOkKK1IYnfGv-r0UBd7KmUjUYKqaJiqvp20/edit#gid=555422107
+
+open https://app.slack.com/client/T0WA5NWKG/G6YK94Q74/details/pins
+
+open https://console.cloud.google.com/compute/instances?project=midsw205&instancessize=50
+
+
+open https://github.com/mids-w205-fund-of-data-eng/course-content
