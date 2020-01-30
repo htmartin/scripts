802a0672daa911ebc6cc4feed006bcc1b17b39f0
htmartin <helen.taylor.martin@gmail.com>
2020-01-30 14:03:40 -0700

    nsf

---
+++ nsf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+
+open https://meetings.nsf.gov/jsp/homepage/panelreview.jsp
+
+open https://lastpass.com/
+
+open https://docs.google.com/document/d/1BT6KMcq6Mzopa9ZzjMqN8QNABf4K4w5LPgkS6miCALs/edit?usp=sharing
+
