c29cfac228ba81dbec5d109fb4c84d90ad3b8afc
htmartin <helen.taylor.martin@gmail.com>
2020-08-24 11:28:22 -0600

    fin

--- fin.sh
+++ fin.sh
@@ -12,8 +12,5 @@ open https://www.usaa.com/inet/ent_logon/Logon?logoffjump=true
 
 open https://www.amazon.com/
 
-open https://connect.secure.wellsfargo.com/auth/login/present
-
 open https://keep.google.com/#home
 
-open https://docs.google.com/spreadsheets/d/1PuhrTLhqpDmVLmIXMWWBQLEbRB8k0LDar_M1G1hyOps/edit#gid=2130294416
