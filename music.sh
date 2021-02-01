070983c1ea029ad618427bdb713579406293b2e5
htmartin <helen.taylor.martin@gmail.com>
2021-02-01 11:45:55 -0700

    trello

---
+++ music.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+open https://sick.console.markmims.com/home/
+
+
+open https://lastpass.com/
+
+open https://sab.console.markmims.com/
+
+open https://dognzb.cr/search
+
+open https://www.how-to-type.com/typing-practice/quote/
---
+++ trello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+open https://trello.com/b/LAynRp5y/finances
+
+
+open https://trello.com/b/m8TLQkqZ/general
+
+open https://trello.com/b/OTdemBmh/guardian
+
+
+open https://trello.com/b/svckJUgp/ssdi
+
+open https://trello.com/b/fcyn0UgZ/mail
