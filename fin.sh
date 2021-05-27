f232942610c89480b67cdacc829fa3afb983ab67
htmartin <mark.mims@ubuntu.com>
2021-05-27 12:02:10 -0600

    fin

--- fin.sh
+++ fin.sh
@@ -3,8 +3,12 @@
 
 
 open https://mint.com/
+#daily
 
 open https://docs.google.com/spreadsheets/d/1PNWLwuLEqcPfP0GZoEp2pD17V6YWGPgsmAWxLoM2R18/edit#gid=111155345
+
+#amex 
+open https://docs.google.com/spreadsheets/d/1YAe3PPtVVyRmam1NC17SS3_XTZllvbjKQCm1El59Bqw/edit#gid=554749653
 
 open https://lastpass.com/
 
@@ -15,3 +19,7 @@ open https://www.amazon.com/
 open https://keep.google.com/#home
 
 open https://console.cloud.google.com/bigquery?project=mmm-ad-finance-b05d
+
+#Amazon
+
+open https://docs.google.com/spreadsheets/d/1CN2abcUmdtfM37zyDXXXBk2R9DMFSYQWf-q3USp1aC0/edit#gid=1853367772
--- trello.sh
+++ trello.sh
@@ -10,6 +10,6 @@ open https://trello.com/b/m8TLQkqZ/general
 open https://trello.com/b/OTdemBmh/guardian
 
 
-open https://trello.com/b/svckJUgp/ssdi
+open https://trello.com/b/zwRblU8q/home-stuff
 
 open https://trello.com/b/fcyn0UgZ/mail
